483a05109961c5b156d6cf295098377fba1c7d76
GXBaum <rafael.beckmann@gmx.de>
2025-04-14 17:53:24 +0200

    alte dateien gelöscht

--- app/src/main/java/com/example/SendMessageDto.kt
+++ app/src/main/java/com/example/SendMessageDto.kt
@@ -1,19 +1,9 @@
 package com.example
 
-data class SendMessageDto(
-    val to: String?,
-    val notification: NotificationBody
-)
-data class NotificationBody(
-    val title: String,
-    val body: String
-)
-
 data class TokenUpdateRequest(
     val token: String,
     val spUsername: String
 )
-
 
 data class UserCourses(
     val courses: List<UserCourse>
